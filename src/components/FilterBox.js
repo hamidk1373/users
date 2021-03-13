ed31ae6ca98134d3bcd775288fb24bd653af10d7
hamid karami <hamidk_1373@yahoo.com>
2021-03-13 15:54:58 +0330

    feat: filter by gender added

--- src/components/FilterBox.js
+++ src/components/FilterBox.js
@@ -1,13 +1,37 @@
 import React from "react";
 
-export default function FilterBox() {
+export default function FilterBox({
+  searchValue,
+  onChange,
+  selectedGender,
+  onChangeSelectedGender,
+}) {
   return (
     <div className='filterBoxContainer'>
       <div className='filterBoxContainer__searchbox'>
         <label>First name</label>
-        <input placeholder='Search in user names...' />
+        <input
+          placeholder='Search in user names...'
+          value={searchValue}
+          onChange={onChange}
+          // onChange={(event)=>onChange(event)}
+        />
       </div>
-      <div className='filterBoxContainer__selectorbox'>Gender filter</div>
+      <div className='filterBoxContainer__selectorbox'>
+        <button
+          onClick={() => onChangeSelectedGender("Male")}
+          className={selectedGender === "Male" ? "selectedGender" : ""}
+        >
+          Male
+        </button>
+        <button
+          onClick={() => onChangeSelectedGender("Female")}
+          className={selectedGender === "Female" ? "selectedGender" : ""}
+        >
+          Female
+        </button>
+        <button onClick={() => onChangeSelectedGender(undefined)}>X</button>
+      </div>
     </div>
   );
 }
--- src/screens/Users.js
+++ src/screens/Users.js
@@ -2,12 +2,42 @@ import FilterBox from "../components/FilterBox";
 import UsersList from "../components/UsersList";
 import { mockData } from "../mockData/users";
 import "./Users.css";
+import { useEffect, useState } from "react";
 
 export default function Users() {
+  const [list, setlist] = useState(mockData);
+  const [filteredList, setfilteredList] = useState(mockData);
+
+  const [searchInputValue, setsearchInputValue] = useState("");
+  const [selectedGender, setselectedGender] = useState(undefined);
+
+  useEffect(() => {
+    let newFilteredList = list;
+
+    // filter by search input
+    newFilteredList = newFilteredList.filter((item) =>
+      item.first_name.toLowerCase().includes(searchInputValue.toLowerCase())
+    );
+
+    // filter by gender
+    if (selectedGender) {
+      newFilteredList = newFilteredList.filter(
+        (item) => item.gender === selectedGender
+      );
+    }
+
+    setfilteredList([...newFilteredList]);
+  }, [searchInputValue, selectedGender]);
+
   return (
     <div>
-      <FilterBox />
-      <UsersList list={mockData} />
+      <FilterBox
+        searchValue={searchInputValue}
+        onChange={(event) => setsearchInputValue(event.target.value)}
+        selectedGender={selectedGender}
+        onChangeSelectedGender={setselectedGender}
+      />
+      <UsersList list={filteredList} />
     </div>
   );
 }
