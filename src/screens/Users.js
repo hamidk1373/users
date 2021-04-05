cb7ece5d5bfc56f0ff269c948294338705af08b1
hamid karami <hamidk_1373@yahoo.com>
2021-04-05 16:23:09 +0430

    fix: fusejs options improved

--- src/screens/Users.js
+++ src/screens/Users.js
@@ -5,6 +5,13 @@ import "./Users.css";
 import { useEffect, useState } from "react";
 import Fuse from "fuse.js";
 
+const options = {
+  threshold: 0.4,
+  keys: ["first_name", "last_name", "email"],
+};
+
+let fuse;
+
 export default function Users() {
   const [list, setlist] = useState(mockData);
   const [filteredList, setfilteredList] = useState(mockData);
@@ -12,29 +19,20 @@ export default function Users() {
   const [searchInputValue, setsearchInputValue] = useState("");
   const [selectedGender, setselectedGender] = useState(undefined);
 
-  const options = {
-    includeScore: true,
-    isCaseSensitive:false,
-    includeMatches:true,
-    minMatchCharLength:3,
-    location:1,
-    keys: ['first_name']
-  };
-  
+  useEffect(() => {
+    fuse = new Fuse(list, options);
+  }, [list]);
 
   useEffect(() => {
-    let newFilteredList = list; 
+    let newFilteredList = list;
 
-     // filter by search input
-    const fuse = new Fuse(newFilteredList, options);
-    if(searchInputValue.length>0)
-    {
-    newFilteredList=fuse.search(searchInputValue);   
-    newFilteredList = newFilteredList.map((res) => res.item);
+    // filter by search input
+    if (searchInputValue.length > 0) {
+      newFilteredList = fuse.search(searchInputValue);
+      newFilteredList = newFilteredList.map((res) => res.item);
     }
 
-    
-   // filter by search input-old
+    // filter by search input-old
     // newFilteredList = newFilteredList.filter((item) =>
     //   item.first_name.toLowerCase().includes(searchInputValue.toLowerCase())
     // );
@@ -47,7 +45,7 @@ export default function Users() {
     }
 
     setfilteredList([...newFilteredList]);
-  }, [searchInputValue, selectedGender]);
+  }, [searchInputValue, selectedGender, list]);
 
   return (
     <div>
